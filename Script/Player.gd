33c56599ffc2b19f883a1bc4737b44d100b600cf
Ricardo Lázaro <caffeine305@gmail.com>
2020-04-03 18:20:50 -0600

    Actualizado Script de Control del Jugador

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,27 +1,81 @@
 extends KinematicBody
 
 var velocity = Vector3(0,0,0)
-const SPEED = 6
+var direction = Vector3(0,0,0)
+var character
+var camera
 
-func _ready(): #kinda like the Unity
-	pass #ends function
+var char_rot
+var angle
+
+const SPEED = 10
+const ACCEL = 5
+const DE_ACCEL = 7
+const GRAVITY = -9.8
+
+func _ready(): #pretty much like Unity's Start() function
+	#camera = get_node(" ") #<-- Averiguar a qué debería apuntar.
+	character = get_node(".")
+	char_rot = character.get_rotation()
+		#pass #ends function
 	
 func _physics_process(delta): #This is the FixedUpdate from Unity, Godot version
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
-		velocity.x = 0
+		direction.x = 0
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
+		direction.x = 1
+	elif Input.is_action_just_released("ui_right"):
+		direction.x = 0
+		
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-	else:
-		velocity.x = lerp(velocity.x,0,0.1)
-
+		direction.x = -1	
+	elif Input.is_action_just_released("ui_left"):
+		direction.x = 0
+	#else:
+	#	direction.x = lerp(direction.x,0,0.1)
+		
+		
 	if Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_up"):
-		velocity.z = 0
+		direction.z = 0
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z = SPEED
+		direction.z = 1
+	elif Input.is_action_just_released("ui_down"):
+		direction.z = 0
+
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z = -SPEED
-	else:
-		velocity.z = lerp(velocity.z,0,0.1)
-	move_and_slide(velocity)
+		direction.z = -1
+		
+	elif Input.is_action_just_released("ui_up"):
+		direction.z = 0
+	#else:
+	#	direction.z = lerp(direction.z,0,0.1)
+	
+		
+	direction.y = 0
+	direction = direction.normalized()
+	
+	velocity.y = delta * GRAVITY 
+	
+	var dv = velocity
+	dv.y = 0
+	
+	var dx = direction * SPEED
+	
+	var accel = DE_ACCEL
+	if (direction.dot(dv) > 0):
+		accel = ACCEL
+		angle = atan2(direction.x,direction.z)
+	#línea 44: el personaje rota "tieso"
+		char_rot.y = angle
+	#línea 45: el personaje rota "suave"
+	#char_rot.y = lerp(char_rot.y,angle,0.1)
+		character.set_rotation(char_rot)
+	
+	dv = dv.linear_interpolate(dx, accel*delta)
+	
+	velocity.x = dv.x
+	velocity.z = dv.z
+
+	velocity = move_and_slide(velocity,Vector3(0,1,0))
+	
+	print(direction)
